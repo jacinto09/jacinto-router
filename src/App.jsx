58efebe5ed28ec95137e75797ef0670242931f37
Jacinto Martinez <jacintomartinez0909@gmail.com>
2023-04-17 13:44:40 +0200

    Lazy load for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
-import Home from './pages/Home'
-import About from './pages/About'
-import Search from './pages/Search'
-import Page404 from './pages/Page404'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import { Router } from './Router'
 import { Route } from './Route'
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+const Search = lazy(() => import('./pages/Search'))
+const Page404 = lazy(() => import('./pages/Page404'))
 
 const AppRoutes = [
   {
@@ -18,10 +19,12 @@ function App () {
   return (
     <>
       <main>
-        <Router routes={AppRoutes} defaultComponent={Page404}>
-          <Route path='/' Component={Home} />
-          <Route path='/about' Component={About} />
-        </Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router routes={AppRoutes} defaultComponent={Page404}>
+            <Route path='/' Component={Home} />
+            <Route path='/about' Component={About} />
+          </Router>
+        </Suspense>
       </main>
     </>
   )
