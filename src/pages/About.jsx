46e6dd6b55e55b367adcebcdb03e742fe71d058a
Jacinto Martinez <jacintomartinez0909@gmail.com>
2023-04-17 13:56:45 +0200

    search link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About () {
   return (
     <div>
       <h1>Esta es la About page</h1>
-      <p>Haciendo un router</p>
+      <p>Hola! Soy Jacinto Martinez, Frontend Developer y estoy creando mi propio Router</p>
       <Link to='/'> Ir a Home</Link>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ export default function Home () {
       <h1>Esta es la Home page</h1>
       <p>Haciendo un router</p>
       <Link to='/about'> Ir a about</Link>
+      <Link to='/search/router'> Ir a search</Link>
     </div>
   )
 }
